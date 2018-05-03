23aeee958ace50a4f9db97fb5e210b2e2a914e53
rnjsgml1997 <huy222956@naver.com>
2018-05-03 16:58:06 +0900

    서버

---
+++ server.py
@@ -0,0 +1,19 @@
+# -*- coding: utf8 -*-
+# 소켓 라이브러리 로딩
+import socket
+# 서버 정보
+info = ("127.0.0.1", 9999)
+# 소켓 생성
+s = socket.socket()
+# 9999번 포트 바인딩
+s.bind(info)
+# 바인딩 포트 리스닝
+s.listen(5)
+# 접속요청 승인
+client, address = s.accept()
+# 클라이언트가 전송한 데이터 수신
+data = client.recv(1024)
+print "address %s send data: %s" % \
+    ( address[0], data)
+# 수신 데이터를 클라이언트에 전송
+client.send(data)
